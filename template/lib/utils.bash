03892acc1a5932dab7b7ba99e28e4555692cb8b4
Dpbm <dpbm136@gmail.com>
2022-04-23 12:50:50 -0300

    modified list all

--- template/lib/utils.bash
+++ template/lib/utils.bash
@@ -2,11 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for <YOUR TOOL>.
-GH_REPO="<TOOL REPO>"
-TOOL_NAME="<YOUR TOOL>"
-TOOL_TEST="<TOOL CHECK>"
-
 fail() {
   echo -e "asdf-$TOOL_NAME: $*"
   exit 1
@@ -14,26 +9,8 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if <YOUR TOOL> is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
-sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if <YOUR TOOL> has other means of determining installable versions.
-  list_github_tags
+  curl https://www.lua.org/ftp/ | grep -Po "(lua-)(([0-9].)+)(tar.gz)" | sort --unique | sed -r "s/.tar.gz//"
 }
 
 download_release() {
